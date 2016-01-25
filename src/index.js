8706ad61472ce4a3d9ce1496c4e655bb9b0d7f60
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2016-01-25 23:57:12 +0300

    add wiring

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles/index.styl'
+
+import { run } from '@motorcycle/core'
+import { makeDOMDriver } from '@motorcycle/dom'
+
+import { App } from 'dialogues/app'
+import { makeLanguageDriver } from 'drivers'
+
+function main(sources) {
+  const app = App(sources)
+
+  return {
+    DOM: app.DOM,
+  }
+}
+
+run(main, {
+  DOM: makeDOMDriver(`#content`),
+  Language: makeLanguageDriver(),
+})
