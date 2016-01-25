27efb38828dd7ecd1fa59bc8525e35b90691021c
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2016-01-25 23:53:59 +0300

    add releases data

---
+++ src/data/releases.js
@@ -0,0 +1,337 @@
+export const releases = [{
+  artist: `FKA Twigs`,
+  release: `M3L155X`,
+  cover: require(`styles/img/covers/twigs.jpg`),
+  position: 1,
+  song: {
+    name: `M3L155X`,
+    link: `bYU3j-22360`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `spotify`,
+    link: `https://open.spotify.com/album/7rSESybi14vAuEVzkipODD`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/m3ll155x-ep/id1028920695`,
+  }],
+}, {
+  artist: `Grimes`,
+  release: `Art Angels`,
+  cover: require(`styles/img/covers/grimes.jpg`),
+  position: 2,
+  song: {
+    name: `Kill v. Maim`,
+    link: `c2EJMd7ZN7w`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `yandex`,
+    link: `https://music.yandex.ru/album/3066814`,
+  }, {
+    site: `spotify`,
+    link: `https://open.spotify.com/album/5hB4jVN4ZHpubyiMmW81K1`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/art-angels/id1051022913`,
+  }],
+}, {
+  artist: `Zebra Katz x Leila`,
+  release: `Nu Renegade`,
+  cover: require(`styles/img/covers/zebra.jpg`),
+  position: 3,
+  song: {
+    name: `You Tell ’Em`,
+    link: `1CV_sZuNRyc`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `soundcloud`,
+    link: `https://soundcloud.com/zebrakatz/sets/nu-renegade-1`,
+  }, {
+    site: `spotify`,
+    link: `https://open.spotify.com/album/3BYqokGvKqZ4hFwkDATsCB`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/nu-renegade-ep/id992302016`,
+  }],
+}, {
+  artist: `SOPHIE`,
+  release: `Product`,
+  cover: require(`styles/img/covers/sophie.jpg`),
+  position: 4,
+  song: {
+    name: `Just Like We Never Said Goodbye`,
+    link: `6Z3N79ktprw`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `youtube`,
+    link: `https://www.youtube.com/watch?v=MVc3Z-bG6Eo&list=PLMdDW3seGrd_5tihi8IkewJtt2BY8Ywdi`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/user/sophiemsmsmsm/playlist/3Q4cPwMHY95ZHXtmcU2xvH`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/product/id1043637917`,
+  }],
+}, {
+  artist: `Arca`,
+  release: `Mutant`,
+  cover: require(`styles/img/covers/arca.jpg`),
+  position: 5,
+  song: {
+    name: `Soichiro`,
+    link: `mYEyHb8x6AY`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `youtube`,
+    link: `https://www.youtube.com/watch?v=S61Ol1LrgFE`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/5JJEl39js1CIZvzrAnX4kw`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/mutant/id1049134484`,
+  }],
+}, {
+  artist: `Mykki Blanco`,
+  release: `Mykki Blanco presents C-ORE`,
+  cover: require(`styles/img/covers/mykki.jpg`),
+  position: 6,
+  song: {
+    name: `Coke White, Starlight`,
+    link: `ioMKTWCIwl8`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `bandcamp`,
+    link: `https://mykkiblanco.bandcamp.com/album/mykki-blanco-presents-c-ore`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/3qRaqlhJoPnPmBtV4OXkGP`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/mykki-blanco-presents-c-ore/id1006598613`,
+  }],
+}, {
+  artist: `Power Pill Fist`,
+  release: `Werebeard`,
+  cover: require(`styles/img/covers/ppfist.jpg`),
+  position: 7,
+  song: {
+    name: `Swear Jar`,
+    link: `AUzpGQ3-kIs`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `bandcamp`,
+    link: `https://powerpillfist.bandcamp.com/album/werebeard`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/werebeard/id1032621092`,
+  }],
+}, {
+  artist: `Chelsea Wolfe`,
+  release: `Abyss`,
+  cover: require(`styles/img/covers/wolfe.jpg`),
+  position: 8,
+  song: {
+    name: `Carrion Flowers`,
+    link: `46u_Ggsub1A`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `bandcamp`,
+    link: `https://chelseawolfe.bandcamp.com/album/abyss`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/7bDxnaImZCM2TJbsI1gMVi`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/abyss/id988559297`,
+  }],
+}, {
+  artist: `Lorn`,
+  release: `Vessel`,
+  cover: require(`styles/img/covers/lorn.jpg`),
+  position: 9,
+  song: {
+    name: `Anvil`,
+    link: `Uniy1C2QruU`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `soundcloud`,
+    link: `https://soundcloud.com/lorn/sets/vessel`,
+  }, {
+    site: `bandcamp`,
+    link: `https://lorn.bandcamp.com/album/vessel`,
+  }],
+}, {
+  artist: `Pixx`,
+  release: `Fall In`,
+  cover: require(`styles/img/covers/pixx.jpg`),
+  position: 10,
+  song: {
+    name: `A Way To Say Goodbye`,
+    link: `j5UQuVVKIg4`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `soundcloud`,
+    link: `https://soundcloud.com/pixxmusic/sets/fall-in-ep`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/4AEX6FHW24qxRaHmywjND0`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/fall-in-ep/id1018264832`,
+  }],
+}, {
+  artist: `Kelela`,
+  release: `Hallucinogen`,
+  cover: require(`styles/img/covers/kelela.jpg`),
+  position: 11,
+  song: {
+    name: `A Message`,
+    link: `m4-9mYrbXVI`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `youtube`,
+    link: `https://www.youtube.com/watch?v=Wee5YAtiPvM`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/4BQjZvl79zW24dc33hNAYk`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/hallucinogen-ep/id1035691174`,
+  }],
+}, {
+  artist: `Jenny Hval`,
+  release: `Apocalypse, girl`,
+  cover: require(`styles/img/covers/hval.jpg`),
+  position: 12,
+  song: {
+    name: `That Battle is Over`,
+    link: `oGksHxwnap0`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `bandcamp`,
+    link: `https://jennyhval.bandcamp.com/album/apocalypse-girl`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/3AeAZfwBgnhmbNEowNFvcB`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/apocalypse-girl/id968903924`,
+  }],
+}, {
+  artist: `HEALTH`,
+  release: `Death Magic`,
+  cover: require(`styles/img/covers/health.jpg`),
+  position: 13,
+  song: {
+    name: `STONEFIST`,
+    link: `G1qvEr9bC6c`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `youtube`,
+    link: `https://www.youtube.com/watch?v=IgAMSMNJ5W8`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/0fXkzTlbfDVSDSXpCSnqLN`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/death-magic/id999941375`,
+  }],
+}, {
+  artist: `Death Grips`,
+  release: `Jenny Death`,
+  cover: require(`styles/img/covers/deathgrips.jpg`),
+  position: 14,
+  song: {
+    name: `I Break Mirrors With My Face in The United States`,
+    link: `pwSvL08isHo`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `soundcloud`,
+    link: `https://soundcloud.com/deathgrips/sets/jenny-death`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/3tWOrJI9fFja5sIk7UiNt1`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/the-powers-that-b/id973715332`,
+  }],
+}, {
+  artist: `Shamir`,
+  release: `Ratchet`,
+  cover: require(`styles/img/covers/shamir.jpg`),
+  position: 15,
+  song: {
+    name: `On The Regular`,
+    link: `Lp9GgdCgMXk`,
+  },
+  text: {
+    ru: ``,
+    en: ``,
+  },
+  links: [{
+    site: `yandex`,
+    link: `https://music.yandex.ru/album/2739906`,
+  }, {
+    site: `spotify`,
+    link: `https://play.spotify.com/album/7vX1WKSgkpG4jwOiaXDVT6`,
+  }, {
+    site: `itunes`,
+    link: `https://itunes.apple.com/us/album/ratchet/id973351333`,
+  }],
+}]
