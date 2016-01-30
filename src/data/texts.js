47fd7001df8d6083c8641cc98c8d307f7e1c92aa
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2016-01-30 23:57:41 +0300

    add texts

---
+++ src/data/texts.js
@@ -0,0 +1,59 @@
+export const texts = {
+  en: {
+    about: `Last year we decided to create our own best releases
+      thing like serious people, we ripped off design of pitchfork,
+      cooked up site and, like, all of that. So here it is: it
+      doesn’t have Lana or that totally overrated XX-guy in it,
+      but instead has all the music that made us laughing, painting,
+      cooking, programming, dancing like apes, crying on the floor
+      and simply browsing the internets like normal kids.`,
+    shamir: `If you want to play jenga, this album is your ideal choice.
+      Also it’s ideal deal for summer hanging and chatting with friends
+      about past, also horny or embarassing things.`,
+    deathgrips: ``,
+    health: ``,
+    hval: ``,
+    kelela: ``,
+    pixx: ``,
+    lorn: `This is why you don’t make list of best releases until
+      the year ends. Something might happen. And it happened:
+      this was released on 30th of December and it is very solid.`,
+    wolfe: ``,
+    ppfist: ``,
+    mykki: ``,
+    arca: ``,
+    sophie: ``,
+    zebra: ``,
+    grimes: ``,
+    twigs: ``,
+  },
+  ru: {
+    about: `В прошлом году мы решили создать лист лучших релизов
+      года как серьезные люди: содрали дизайн с пичфорка,
+      сделали сайт и все такое. Это он — в нем нет Ланы
+      или этого вашего скучного жами́ хэхэ, зато есть куча музыки,
+      под которую мы смеялись, рисовали, готовили, работали,
+      танцевали как обезьяны, ревели на полу и просто втыкали
+      в интернетики как обычные хикки.`,
+    shamir: `Если вдруг захочется поиграть в дженгу, то альбом
+      идеально подходит. Кроме того идеально подходит для лета
+      и болтовни с друзьями о прошлом, пошлом и стеснительном.`,
+    deathgrips: ``,
+    health: ``,
+    hval: ``,
+    kelela: ``,
+    pixx: ``,
+    lorn: `Вот причина, почему составлять список лучших релизов до конца
+      года — бессмысленное занятие. Всегда может что-нибудь случиться.
+      Например, этот альбом, который случился 30 декабря, и случился
+      он довольно крутым.`,
+    wolfe: ``,
+    ppfist: ``,
+    mykki: ``,
+    arca: ``,
+    sophie: ``,
+    zebra: ``,
+    grimes: ``,
+    twigs: ``,
+  }
+}
